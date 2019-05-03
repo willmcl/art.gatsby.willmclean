695c6241a4049c2b0dfac4d9df35b6f456922e2e
Will McLean <willmcl@gmail.com>
2019-05-03 15:07:26 +1000

    Update the site details.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Great Start`,
+    description: `Will's great start for Gatsby builds`,
+    author: `Will McLean`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
