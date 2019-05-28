5c9412bafc4eef66e5cc16f59a1eab874debe097
Will McLean <willmcl@gmail.com>
2019-05-28 18:32:54 +1000

    Create pages for both art and for blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,11 @@ const path = require("path");
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
-  const postTemplate = path.resolve(`src/templates/Blog.js`);
+  const blogTemplate = path.resolve(`src/templates/Blog.js`);
+  const artTemplate = path.resolve(`src/templates/Art.js`);
   return graphql(`
     {
-      allMarkdownRemark(
+      blogs: allMarkdownRemark(
         filter: { fileAbsolutePath: {regex : "\/_posts/blog/"} }
         sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
@@ -19,7 +20,19 @@ exports.createPages = ({ actions, graphql }) => {
         edges {
           node {
             frontmatter {
-              path
+              title
+            }
+          }
+        }
+      }
+      art: allMarkdownRemark(
+        filter: { fileAbsolutePath: {regex : "\/_posts/art/"} }
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
               title
             }
           }
@@ -27,20 +40,37 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
+
     if (result.errors) {
       return Promise.reject(result.errors)
     }
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+
+    // Create blog pages
+    result.data.blogs.edges.forEach(({ node }) => {
       createPage({
         path: '/' + node.frontmatter.title.toLowerCase()
         .replace(/[^\w ]+/g,'')
         .replace(/ +/g,'-'),
-        component: postTemplate,
+        component: blogTemplate,
         context: {
           title: node.frontmatter.title,
         }, // additional data can be passed via context
       })
-    })
+    });
+
+    // Create art pages
+    result.data.art.edges.forEach(({ node }) => {
+      createPage({
+        path: '/' + node.frontmatter.title.toLowerCase()
+        .replace(/[^\w ]+/g,'')
+        .replace(/ +/g,'-'),
+        component: artTemplate,
+        context: {
+          title: node.frontmatter.title,
+        }, // additional data can be passed via context
+      })
+    });
+
   })
 };
 
