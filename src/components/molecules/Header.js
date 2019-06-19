9d163a7562c4c70065027b25dee0ec89d367edb2
Will McLean <willmcl@gmail.com>
2019-06-19 17:16:32 +1000

    Small changes.

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
             {this.props.siteTitle}
           </Link>
         </p>
-        <p>Art Archive</p>
+        <p>Archive</p>
       </Holder>
     )
   }
