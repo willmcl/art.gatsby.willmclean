7b7d72676887ec714e88bb016c853a6761d3a82a
Will McLean <willmcl@gmail.com>
2019-07-11 10:12:14 +1000

    Style the blog posts.

--- src/components/molecules/BlogPreview.js
+++ src/components/molecules/BlogPreview.js
@@ -2,16 +2,63 @@ import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import { convertToSlug } from '../../utils/helpers';
 import FirstBlogArtImage from './FirstBlogArtImage';
+import styled from 'styled-components';
+
+const Holder = styled.li`
+  @media ( ${props => props.theme.breakpoints.sm} ) {
+    grid-column: 2/3;
+  }
+  .text{
+    text-align: justify;
+    line-height: 1.2;
+    a {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      ${props => props.theme.typeStyles( 2 )};
+      .number {
+        font-weight: normal;
+        margin-right: 1rem;
+        font-style: italic;
+        ${props => props.theme.typeStyles( -8 )};
+      }
+    }
+  }
+  .images {
+    display: none;
+    img {
+      position: fixed;
+      z-index: -1;
+      width: 20vw;
+      height: auto;
+      &:nth-child(1) { top: 8vw; left: 8vw; }
+      &:nth-child(2) { top: 8vw; right: 8vw; }
+      &:nth-child(3) { bottom: 8vw; right: 8vw; }
+      &:nth-child(4) { bottom: 8vw; left: 8vw; }
+    }
+  }
+  .text:hover + .images { display: block; }
+`;
 
 class BlogPreview extends Component {
 
   render() {
     const frontmatter = this.props.post.frontmatter;
     return (
-      <li>
-        <Link to={`/blog/${convertToSlug(frontmatter.title)}`}>{frontmatter.title}</Link>
-        <FirstBlogArtImage art={frontmatter.art}/>
-      </li>
+      <Holder>
+        <div className="text">
+          <Link to={`/blog/${convertToSlug( frontmatter.title )}`}>
+            <span className="number">{this.props.i + 1}</span>
+            <span>{frontmatter.title}</span>
+          </Link>
+        </div>
+        <div className="images">
+          <FirstBlogArtImage art={frontmatter.art}/>
+          <FirstBlogArtImage art={frontmatter.art}/>
+          <FirstBlogArtImage art={frontmatter.art}/>
+          <FirstBlogArtImage art={frontmatter.art}/>
+        </div>
+      </Holder>
     )
   }
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,9 +7,15 @@ import SEO from '../components/molecules/SEO';
 const List = styled.ul`
   list-style: none;
   padding-left: 0;
-  margin-bottom: 8rem;
+  margin: 8rem 0;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-row-gap: 2rem;
+  @media ( ${props => props.theme.breakpoints.sm} ) {
+    grid-template-columns: 1fr 4fr 1fr;
+  }
   @media ( ${props => props.theme.breakpoints.md} ) {
-    margin-left: 50%;
+    grid-template-columns: 1fr 2fr 1fr;
   }
 `;
 
@@ -20,8 +26,8 @@ class Blog extends Component {
         <SEO title="Blog"/>
         <article>
           <List>
-            {this.props.data.allMarkdownRemark.edges.map( edge => (
-              <BlogPreview key={edge.node.id} post={edge.node}/>
+            {this.props.data.allMarkdownRemark.edges.map( (edge, i) => (
+              <BlogPreview key={edge.node.id} post={edge.node} i={i}/>
             ) )}
           </List>
         </article>
